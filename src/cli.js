f41731d6877b272470111b1d21cac26beb9f5e3e
Mortuz Alam <mortuz@Mortuzs-Mac-mini.local>
2020-07-20 10:05:06 +0530

    add graphql typescript

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import arg from 'arg'
-import inquirer from 'inquirer';
-import { createProject } from './main';
-import { version } from '../package.json';
+import inquirer from 'inquirer'
+import { createProject } from './main'
+import { version } from '../package.json'
 
 function parseArgumentsInOptions(rawArguments) {
   const args = arg(
@@ -20,8 +20,8 @@ function parseArgumentsInOptions(rawArguments) {
     {
       argv: rawArguments.splice(2)
     }
-  );
-
+  )
+  console.log(args)
   return {
     skipPrompts: args['--yes'] || false,
     git: args['--git'] || false,
@@ -29,17 +29,14 @@ function parseArgumentsInOptions(rawArguments) {
     template: args['--template'],
     noInstall: args['--no-install'] || false,
     version: args['--version'] || false,
-    help: args['--help'] || false,
+    help: args['--help'] || false
   }
 }
 
-const choices = [
-  'express-api',
-  'html'
-]
+const choices = ['express-api', 'graphql-typescript', 'html']
 
 async function promptForMissingOptions(options) {
-  const defaultTemplate = 'express-api';
+  const defaultTemplate = 'express-api'
 
   if (options.skipPrompts) {
     return {
@@ -48,29 +45,29 @@ async function promptForMissingOptions(options) {
     }
   }
 
-  const questions = [];
+  const questions = []
 
   if (!options.name) {
-
     questions.push({
-      name: "name",
-      type: "input",
-      message: "what is the name of your project?\n",
+      name: 'name',
+      type: 'input',
+      message: 'what is the name of your project?\n',
       validate: function (input) {
-        if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
-        else
-          return "Project name may only include letters, numbers, underscores and hashes.";
+        if (/^([A-Za-z\-\_\d])+$/.test(input)) {
+          return true
+        } else {
+          return 'Project name may only include letters, numbers, underscores and hashes.'
+        }
       }
     })
   }
-
 
   if (!options.template) {
     questions.push({
       type: 'list',
       name: 'template',
       message: 'Please choose a template to use',
-      choices,
+      choices
     })
   }
 
@@ -83,20 +80,21 @@ async function promptForMissingOptions(options) {
     })
   }
 
-  const answers = await inquirer.prompt(questions);
+  const answers = await inquirer.prompt(questions)
+  console.log(answers)
   return {
-    ...options,
+    ...answers,
     template: options.template || answers.template,
     git: options.git || answers.git
-  };
+  }
 }
 
 export async function cli(args) {
-  let options = parseArgumentsInOptions(args);
+  let options = parseArgumentsInOptions(args)
   if (options.version) {
-    return console.log(version);
+    return console.log(version)
   } else if (options.help) {
-    return console.log('show help options');
+    return console.log('show help options')
   }
   options = await promptForMissingOptions(options)
   await createProject(options)
--- src/main.js
+++ src/main.js
@@ -1,52 +1,55 @@
-import chalk from 'chalk';
-import fs from 'fs';
-import ncp from 'ncp';
-import path from 'path';
-import { promisify } from 'util';
-import execa from 'execa';
-import Listr from 'listr';
-import fse from 'fs-extra';
-import { projectInstall } from 'pkg-install';
-import axios from 'axios';
-import { version, name } from '../package.json';
+import chalk from 'chalk'
+import fs from 'fs'
+import ncp from 'ncp'
+import path from 'path'
+import { promisify } from 'util'
+import execa from 'execa'
+import Listr from 'listr'
+import fse from 'fs-extra'
+import { projectInstall } from 'pkg-install'
+import axios from 'axios'
+import { version, name } from '../package.json'
 
-const access = promisify(fs.access);
-const copy = promisify(ncp);
+const access = promisify(fs.access)
+const copy = promisify(ncp)
 
-const CURR_DIR = process.cwd();
+const CURR_DIR = process.cwd()
 
 async function initGit(options) {
   const result = await execa('git', ['init'], {
     cwd: `${CURR_DIR}/${options.name}`
-  });
+  })
 
   if (result.failed) {
-    return Promise.reject(new Error('Failed to initialize Git'));
+    return Promise.reject(new Error('Failed to initialize Git'))
   }
-  return;
+  return
 }
 
 async function copyTemplateFiles(options) {
+  console.log(options)
   return copy(options.templateDirectory, options.targetDirectory, {
     clobber: false
   })
 }
 
 async function configurePackageDotJson(options) {
-  let filePath = `${options.targetDirectory}/package.json`;
-  let rawData = fs.readFileSync(filePath);
-  let jsonData = JSON.parse(rawData);
-  jsonData.name = options.name;
-  fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+  let filePath = `${options.targetDirectory}/package.json`
+  let rawData = fs.readFileSync(filePath)
+  let jsonData = JSON.parse(rawData)
+  jsonData.name = options.name
+  fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2))
 }
 
 async function checkUpdate() {
   const { data } = await axios.get(`https://www.npmjs.com/search/suggestions?q=${name}`)
-  const packageInfo = data[0];
+  const packageInfo = data[0]
 
   if (packageInfo.scope === 'idevia' && packageInfo.version !== version) {
     console.log(`
-    ${chalk.magenta.bold('v' + packageInfo.version)} is available. To update run the following command
+    ${chalk.magenta.bold(
+      'v' + packageInfo.version
+    )} is available. To update run the following command
 
     ${chalk.cyan.bold('npm install @idevia/create-project -g')}
     `)
@@ -54,24 +57,23 @@ async function checkUpdate() {
 }
 
 export async function createProject(options) {
-
   options = {
     ...options,
     targetDirectory: `${CURR_DIR}/${options.name}`
-  };
+  }
 
   const templateDir = path.resolve(
     __dirname,
     '../templates',
     options.template.toLowerCase()
-  );
-  options.templateDirectory = templateDir;
+  )
+  options.templateDirectory = templateDir
 
   try {
-    await access(templateDir, fs.constants.R_OK);
+    await access(templateDir, fs.constants.R_OK)
   } catch (err) {
-    console.error('%s Invalid template name', chalk.red.bold('ERROR'));
-    process.exit(1);
+    console.error('%s Invalid template name', chalk.red.bold('ERROR'))
+    process.exit(1)
   }
 
   const tasks = new Listr([
@@ -90,20 +92,21 @@ export async function createProject(options) {
     },
     {
       title: 'Installing dependencies',
-      task: () => projectInstall({
-        cwd: options.targetDirectory
-      }),
+      task: () =>
+        projectInstall({
+          cwd: options.targetDirectory
+        }),
       skip: () => options.noInstall
     }
   ])
 
   try {
-    await tasks.run();
+    await tasks.run()
   } catch (err) {
-    fse.emptyDirSync(options.targetDirectory);
-    fse.removeSync(options.targetDirectory);
-    throw err;
+    fse.emptyDirSync(options.targetDirectory)
+    fse.removeSync(options.targetDirectory)
+    throw err
   }
-  await checkUpdate();
-  return true;
+  await checkUpdate()
+  return true
 }
